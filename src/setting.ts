7bf1344e9cab3e075d12e400faf3ba2902489cbc
uedake <uedakensuke722@gmail.com>
2025-10-06 09:29:44 +0900

    change cognito client

--- src/setting.ts
+++ src/setting.ts
@@ -1,6 +1,6 @@
 export const cognitoAuthConfig = {
   authority: "https://cognito-idp.ap-northeast-1.amazonaws.com/ap-northeast-1_kw2EVel03",
-  client_id: "44cno5628t00u1a094smvbmmhm",
+  client_id: "1ar8e89c954jnk9qm2974g2iag",
   redirect_uri: `${window.location.protocol}//${window.location.host}/`,
   response_type: "code",
   scope: "openid email profile",
